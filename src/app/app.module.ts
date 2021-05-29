e2729b22e4e06c07e0f19af06e179d0dc41711e8
BorkowskiMichalCo <michal.borkowski.co@gmail.com>
2021-05-29 13:06:55 +0200

    Trading search coingecko

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
@@ -15,6 +16,7 @@ import { InfoComponent } from './login/info/info.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MainCardsComponent } from './components/main-cards/main-cards.component';
+import { CryptoService } from './services/crypto.service';
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, RegisterComponent, InfoComponent, SidebarComponent, HeaderComponent, MainCardsComponent],
@@ -25,8 +27,9 @@ import { MainCardsComponent } from './components/main-cards/main-cards.component
     AngularFireDatabaseModule,
     FormsModule,
     AngularFirestoreModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [CryptoService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/components/main-cards/main-cards.component.ts
+++ src/app/components/main-cards/main-cards.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { CryptoService } from '../../services/crypto.service';
 
 @Component({
   selector: 'app-main-cards',
   templateUrl: './main-cards.component.html',
-  styleUrls: ['./main-cards.component.scss']
+  styleUrls: ['./main-cards.component.scss'],
 })
 export class MainCardsComponent implements OnInit {
+  names: any = [];
 
-  constructor() { }
+  constructor(private name: CryptoService) {}
 
   ngOnInit(): void {
+    this.name.getName().subscribe(data => {
+      console.log(data);
+      this.names = data.coins;
+    });
   }
-
 }
